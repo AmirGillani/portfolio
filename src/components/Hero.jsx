02affe4465696dbba260c3af20b0df0c9c73c6d7
amir <amirgillani160@gmail.com>
2025-01-31 17:36:12 +0500

    Amir changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import hero from "../assets/assets/hero.jpg";
+import hero from "../assets/assets/hero2.jpg";
 
 import { PROFILE } from "../assets/constants/index";
 
@@ -8,7 +8,7 @@ import { motion } from "framer-motion";
 
 export default function Hero() {
   return (
-    <div className="h-screen relative flex justify-center items-end" id="hero">
+    <div className="md:h-screen h-[50vh] relative flex justify-center items-end" id="hero">
       <motion.img
         src={hero}
         alt={"Hero"}
@@ -31,7 +31,9 @@ export default function Hero() {
         animate={{ opacity: 1 }}
         transition={{ duration: 1, delay: 1 }}
       >
-        <h1 className="text-5xl text-white tracking-wider uppercase font-semibold md:text-7xl">
+        <h1 className="text-5xl text-white tracking-wider uppercase font-semibold md:text-7xl" style={{
+    textShadow: "0 0 5px #ff00ff, 0 0 10px #ff00ff, 0 0 20px #ff00ff, 0 0 40px #ff00ff, 0 0 80px #ff00ff, 0 0 90px #ff00ff, 0 0 100px #ff00ff"
+  }}>
           {PROFILE.name}
         </h1>
         <p className="pt-2 font-semibold">{PROFILE.info}</p>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import logo from "../assets/assets/logo.jpeg";
+import logo from "../assets/assets/logo.png";
 
 import { RxHamburgerMenu } from "react-icons/rx";
 
@@ -56,7 +56,7 @@ export default function Navbar() {
 
       <div className="mx-auto gap-4 hidden max-w-2xl items-center justify-center rounded-lg bg-black/20 p-3 backdrop-blur-lg lg:flex">
         <a href="#">
-          <img src={logo} style={{ width: "60px" }} alt="logo" />
+          <img src={logo} className="md:w-24 w-10" alt="logo" />
         </a>
         <ul className="flex items-center gap-4">
           {NAVIGATION_LINKS.map((link, index) => {
@@ -86,7 +86,7 @@ export default function Navbar() {
             <a href="#">
               <img
                 src={logo}
-                style={{ width: "90px" }}
+                style={{ width: "60px" }}
                 alt="logo"
                 className="m-2"
               />
